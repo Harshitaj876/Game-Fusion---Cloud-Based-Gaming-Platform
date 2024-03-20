d63893f160807def87c1734dcb672b6ac4f7c721
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-03-20 12:36:33 +0530

    modified:   src/App.css 	new file:   src/Assets/LoginImage.png 	modified:   src/Pages/Login/Login.css 	modified:   src/Pages/Login/Login.js

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import FrontImage from '../../Assets/LoginImage.png'
+import Logo from '../../Assets/Logo.png'
+import '../Login/Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='Login'>
+    <div className='loginform'>
+      <img src={Logo} alt='Logo'></img>
+      <h1 className='LoginText'>Login</h1>
+    </div>
+      <img src={FrontImage} alt="FrontImage"></img>
+    </div>
   )
 }
 
