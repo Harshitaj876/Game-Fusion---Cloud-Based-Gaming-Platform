2fa85318bb91d889310865dde803b3a3ba412f2d
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-03-21 12:01:06 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   src/Assets/Right_arrow.png 	new file:   src/Assets/discord.png 	new file:   src/Assets/google.png 	modified:   src/Pages/Login/Login.css 	modified:   src/Pages/Login/Login.js 	new file:   src/Pages/SignUp/Signup.css 	new file:   src/Pages/SignUp/Signup.js

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,15 +2,41 @@ import React from 'react'
 import FrontImage from '../../Assets/LoginImage.png'
 import Logo from '../../Assets/Logo.png'
 import '../Login/Login.css'
+import Discord from '../../Assets/discord.png'
+import Google from '../../Assets/google.png'
 
 const Login = () => {
+
   return (
     <div className='Login'>
-    <div className='loginform'>
-      <img src={Logo} alt='Logo'></img>
-      <h1 className='LoginText'>Login</h1>
-    </div>
-      <img src={FrontImage} alt="FrontImage"></img>
+      <div className='form'>
+        <div className='LogoSection'>
+          <img src={Logo} alt='logo'></img>
+          <h1>Login</h1>
+        </div>
+        <div className='Options'>
+          <div className='buttons'>
+            <button><img src={Discord} alt='Discord'></img></button>
+            <button><img src={Google} alt='Google'></img></button>
+          </div>
+          <div className="text-with-lines">
+            <div className="horizontal-line"></div>
+            <div className="blackText">OR</div>
+            <div className="horizontal-line"></div>
+          </div>
+
+        </div>
+        <div className='Inputs'>
+          <input type="text" id="Username" name="Username" placeholder="Username"></input>
+          <input type="Password" id="Password" name="Password" placeholder="Password"></input>
+          <div className='space'>
+            Forgot Password?
+            <button>Login</button>
+          </div>
+          <button className='normal'>Create account</button>
+        </div>
+      </div>
+      <img src={FrontImage} alt='frontImage'></img>
     </div>
   )
 }
---
+++ src/Pages/SignUp/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../SignUp/Signup.css'
+
+const Signup = () => {
+  return (
+    <div className='whole'>
+        <h1>Vishwam</h1>
+    </div>
+  )
+}
+
+export default Signup
