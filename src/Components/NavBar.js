5808ce52393f17f0cf7cc89ac16786a1fa3c6da2
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-03-26 13:22:01 +0530

    modified:   src/App.css 	new file:   src/Assets/home_inactive.png 	modified:   src/Components/NavBar.css 	modified:   src/Components/NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,8 @@
-import React from "react";
-import {Link } from "react-router-dom";
+import React, {useState} from "react";
+import {NavLink} from "react-router-dom";
 import "../Components/NavBar.css"
 import Logo from '../Assets/logo_navbar.png';
-import Home_active from '../Assets/Home_active.png'
+import Home_inactive from '../Assets/home_inactive.png'
 import trending_inactive from '../Assets/trending_inactive.png'
 import games_category_inactive from '../Assets/games_category_inactive.png'
 import esports_inactive from '../Assets/esports_inactive.png'
@@ -12,21 +12,47 @@ import settings_inactive from '../Assets/settings_inactive.png'
 import helpsupport_inactive from '../Assets/helpsupport_inactive.png'
 
 function NavBar() {
+  
+  const [activeLink, setActiveLink] = useState('');
+
+  const handleSetActiveLink = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <nav className="nav">
       <div className="Conatiner">
         <img src={Logo} alt="logo"></img>
-        <div className="Menu1">
-          <Link to='/' className="link"><span><img src={Home_active} alt=""/></span>Home</Link>
-          <Link to='/trending' className="link"><span><img src={trending_inactive} alt=""/></span>Trending</Link>
-          <Link to='/gamescategory' className="link"><span><img src={games_category_inactive} alt=""/></span>Games Category</Link>
-          <Link to='/esports' className="link"><span><img src={esports_inactive} alt=""/></span>Esports</Link>
-          <Link to='/downloadapp' className="link"><span><img src={download_inactive} alt=""/></span>Download App</Link>
-          <Link to='/streams' className="link"><span><img src={streams_inactive} alt=""/></span>Streams</Link>
-        </div>
+        <ul>
+        <li>
+          <NavLink to="/" activeClassName="active" onClick={() => handleSetActiveLink('home')} className="link"><span><img src={Home_inactive} alt=""></img></span>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/trending" activeClassName="active" onClick={() => handleSetActiveLink('trending')} className="link"><span><img src={trending_inactive} alt=""></img></span>Trending</NavLink>
+        </li>
+        <li>
+          <NavLink to="/gamescategory" activeClassName="active" onClick={() => handleSetActiveLink('GamesCategory')} className="link"><span><img src={games_category_inactive} alt=""></img></span>Games Category</NavLink>
+        </li>
+        <li>
+          <NavLink to="/esports" activeClassName="active" onClick={() => handleSetActiveLink('Esports')} className="link"><span><img src={esports_inactive} alt=""></img></span>Esports</NavLink>
+        </li>
+        <li>
+          <NavLink to="/downloadapp" activeClassName="active" onClick={() => handleSetActiveLink('DownloadApp')} className="link"><span><img src={download_inactive} alt=""></img></span>Download App</NavLink>
+        </li>
+        <li>
+          <NavLink to="/streams" activeClassName="active" onClick={() => handleSetActiveLink('Streams')} className="link"><span><img src={streams_inactive} alt=""></img></span>Streams</NavLink>
+        </li>
+        
+      </ul>
         <div className="Menu2">
-          <Link to='/settings' className="link"><span><img src={settings_inactive} alt=""/></span>Settings</Link>
-          <Link to='/help&support' className="link"><span><img src={helpsupport_inactive} alt=""/></span>Help and support</Link>
+          <ul>
+            <li>
+              <NavLink to="/settings" activeClassName="active" onClick={() => handleSetActiveLink('settings')} className="link"><span><img src={settings_inactive} alt=""></img></span>Settings</NavLink>
+            </li>
+            <li>
+              <NavLink to="/help&support" activeClassName="active" onClick={() => handleSetActiveLink('help')} className="link"><span><img src={helpsupport_inactive} alt=""></img></span>Help and Support</NavLink>
+            </li>
+          </ul>  
         </div>
       </div>
     </nav>
