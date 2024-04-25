cfa3ab1b249e9b01d34f2495f78e0763667b4592
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-04-25 20:21:13 +0530

    new file:   src/Assets/Word search.png 	new file:   src/Assets/card.png 	new file:   src/Assets/casino.png 	new file:   src/Assets/deadshot.png 	new file:   src/Assets/gun.png 	new file:   src/Assets/jackpot.png 	new file:   src/Assets/remote.png 	modified:   src/Pages/Home/Home.css 	modified:   src/Pages/Home/Home.js 	modified:   src/Tetris/TetrisGame.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,6 +8,13 @@ import Notification from '../../Assets/notification.png'
 import Snakes from '../../Assets/snake_poster.png'
 import Tetris from '../../Assets/tetris.png'
 import TicTacToe_Poster from '../../Assets/TicTacToe_poster.png'
+import casino from '../../Assets/casino.png'
+import Jackpot from '../../Assets/jackpot.png'
+import remote from '../../Assets/remote.png'
+import Cards from '../../Assets/card.png'
+import gun from '../../Assets/gun.png'
+import wordSearch from '../../Assets/Word search.png'
+import DeadShot from '../../Assets/deadshot.png'
 
 const Home = () => {
   const [user, setUser] = useState(null);
@@ -58,9 +65,41 @@ const Home = () => {
       </div>
 
       <div className='Genres'>
-          <div className='gen1'>
-
-          </div>
+          <button className='gen1'>
+            <img src={casino} alt=''></img>
+            <div className='rightText'>
+              <h5>Crypto Casino</h5>
+              <h4>Crypto Casino games</h4>
+            </div>
+          </button>
+          <button className='gen1'>
+            <img src={remote} alt=''></img>
+            <div className='rightText'>
+              <h5>Games</h5>
+              <h4>Blockchain make games</h4>
+            </div>
+          </button>
+          <button className='gen1'>
+            <img src={Jackpot} alt=''></img>
+            <div className='rightText'>
+              <h5>Jackpot</h5>
+              <h4>Jackpots to play online</h4>
+            </div>
+          </button>
+          <button className='gen1'>
+            <img src={Cards} alt=''></img>
+            <div className='rightText'>
+              <h5>Poker</h5>
+              <h4>Worlds most popular</h4>
+            </div>
+          </button>
+          <button className='gen1'>
+            <img src={gun} alt=''></img>
+            <div className='rightText'>
+              <h5>Action</h5>
+              <h4>Shooting Games</h4>
+            </div>
+          </button>
       </div>
 
       <div className='Games'>
@@ -86,18 +125,11 @@ const Home = () => {
 
       <div className='Games'>
         Popular Games
-        <div className='gameContent'>
+        <div className='gameContentexp'>
           <Link to="/game">
           <button className='G1'>
-            <img src={TicTacToe_Poster} alt=''></img>
-            <div className='title'>Tic Tac Toe</div>
-          </button>
-          </Link>
-
-          <Link to="/game2">
-          <button className='G1'>
-            <img src={Snakes} alt=''></img>
-            <div className='title'>Snakes</div>
+            <img src={wordSearch} alt=''></img>
+            <div className='title'>Word Search</div>
           </button>
           </Link>
 
@@ -105,6 +137,20 @@ const Home = () => {
           <button className='G1'>
             <img src={Tetris} alt=''></img>
             <div className='title'>Tetris</div>
+          </button>
+          </Link>
+
+          <Link to="/game2">
+          <button className='G1'>
+            <img src={DeadShot} alt=''></img>
+            <div className='title'>Dead Shot</div>
+          </button>
+          </Link>
+
+          <Link to="/game2">
+          <button className='G1'>
+            <img src={Snakes} alt=''></img>
+            <div className='title'>Snakes</div>
           </button>
           </Link>
 
