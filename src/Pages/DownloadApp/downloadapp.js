f06aeb19779f72381062fb3404fc1c78cd76fdac
Harshitaj876 <71326956+Harshitaj876@users.noreply.github.com>
2024-04-26 12:47:44 +0530

    new file:   src/Assets/Download.png 	new file:   src/Pages/DownloadApp/downloadapp.css 	modified:   src/Pages/DownloadApp/downloadapp.js

--- src/Pages/DownloadApp/downloadapp.js
+++ src/Pages/DownloadApp/downloadapp.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../DownloadApp/downloadapp.css'
+import download from '../../Assets/Download.png'
 
 const downloadapp = () => {
   return (
-    <div>downloadapp</div>
+    <div className='whole'>
+      <img src={download} alt=''></img>
+      <div className='head'>
+        <h1>Revolutionize Your Online Experience with Game Fusion</h1>
+        <h5>Your Website, Now in Your Pocket!</h5>
+        <h5>Stay connected on-the-go with our upcoming mobile app, Game Fusion. Seamlessly access all the features of our website right from your smartphone. Don't miss out—download the app and elevate your online experience today!</h5>
+      </div>
+    </div>
   )
 }
 
